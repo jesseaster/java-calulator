0437dd0c8f4801d7fec453fa0d60a419228de39f
kikiloverafa <j.l.kiki.shen@gmail.com>
2014-11-02 21:43:17 -0500

    GUI modified

--- calculator/KeyboardExpressionCalculator.java
+++ calculator/KeyboardExpressionCalculator.java
@@ -1,9 +1,47 @@
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.lang.Math;
 
-public class KeyboardExpressionCalculator implements ActionListener
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+public class KeyboardExpressionCalculator implements ActionListener, Accumulator
 {
+	 
+	//added by Kiki
+	 JFrame  window      = new JFrame("Complex Expression Calculator");
+	 JButton clearButton = new JButton("CLEAR"); 
+	 JLabel amountLabel = new JLabel("Enter amount", SwingConstants.RIGHT);
+	 JLabel totalLabel = new JLabel("Total", SwingConstants.RIGHT);
+		JTextField leftTextField = new JTextField(8);
+		JTextField rightTextField = new JTextField(8);
+		JTextField totalTextField = new JTextField(8);
+		JPanel panel = new JPanel();
+		JTextField errorTextField = new JTextField(32);
+		JCheckBox checkBox= new JCheckBox("Drop.00");
+		JTextArea logTextArea = new JTextArea(20,40);
+		JScrollPane logScrollPane = new JScrollPane(logTextArea);
+		String newLine = System.lineSeparator();
+		JTextField xValue = new JTextField(8);
+		static JRadioButton accumulatorMode = new JRadioButton();
+		static JRadioButton expressionMode = new JRadioButton();
+		static JRadioButton graphMode = new JRadioButton();
+		ButtonGroup buttonGroup = new ButtonGroup();
+		
+		
 public static void main(String[] args) throws Exception
  {
  BufferedReader br = new BufferedReader(
@@ -16,20 +54,8 @@ public static void main(String[] args) throws Exception
  double result      = 0;
  int    i           = 0;
  
- //added by Kiki
- JFrame  window      = new JFrame("Complex Expression Calculator");
- JButton clearButton = new JButton("CLEAR"); 
- JLabel amountLabel = new JLabel("Enter amount", SwingConstants.RIGHT);
- JLabel totalLabel = new JLabel("Total", SwingConstants.RIGHT);
-	JTextField amountTextField = new JTextField(8);
-	JTextField totalTextField = new JTextField(8);
-	JPanel panel = new JPanel();
-	JTextField errorTextField = new JTextField(32);
-	JCheckBox checkBox= new JCheckBox("Drop.00");
-	JTextArea logTextArea = new JTextArea(20,40);
-	JScrollPane logScrollPane = new JScrollPane(logTextArea);
-	String newLine = System.lineSeparator();
-	
+ if(expressionMode.isSelected() == true )
+ {
  System.out.println("Enter a simple expression (single operator + - * /)");
  while (true)
    {
@@ -72,6 +98,7 @@ public static void main(String[] args) throws Exception
    System.out.println(" = " + result);
    }
  }
+ }
 
 //added by Kiki
 public KeyboardExpressionCalculator()
@@ -79,26 +106,35 @@ public KeyboardExpressionCalculator()
 	panel.setLayout(new GridLayout(1,5));
 	panel.add(clearButton);
 	panel.add(amountLabel);
-	panel.add(amountTextField);
+	panel.add(leftTextField);
+	panel.add(rightTextField);
 	panel.add(totalLabel);
 	panel.add(totalTextField);
 	panel.add(checkBox);
+	buttonGroup.add(accumulatorMode);
+	buttonGroup.add(expressionMode);
+	buttonGroup.add(graphMode);
+	panel.add(accumulatorMode);
+	panel.add(expressionMode);
+	panel.add(graphMode);
 	window.getContentPane().add(panel,"North");
 	window.setSize(600, 300);
 	window.setLocation(300,200);
 	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	window.setVisible(true);
 	totalTextField.setEditable(false);
-	totalTextField.setFont(new Font("Times Roman", Font.BOLD, 20));
+	//totalTextField.setFont(new Font("Times Roman", Font.BOLD, 20));
 	clearButton.addActionListener(this);
-	amountTextField.addActionListener(this);
+	leftTextField.addActionListener(this);
+	rightTextField.addActionListener(this);
 	window.getContentPane().add(errorTextField, "South");
 	window.getContentPane().add(logScrollPane, "Center");
 	errorTextField.setEditable(false);
 	logTextArea.setEditable(false);
-	logTextArea.setFont(new Font("Times Roman", Font.BOLD, 20));
-	amountTextField.requestFocus();
+	//logTextArea.setFont(new Font("Times Roman", Font.BOLD, 20));
+	leftTextField.requestFocus();
 }
+
 
 public void actionPerformed(ActionEvent ae) {
 	if(ae.getSource()== clearButton)
@@ -106,19 +142,19 @@ public void actionPerformed(ActionEvent ae) {
 		clear();
 		return;
 		}
-	if(ae.getSource()== amountTextField)
+	if(ae.getSource()== leftTextField)
 	{
 	try{
-		String enteredAmount = amountTextField.getText();
+		String enteredAmount = leftTextField.getText();
 		String newTotal = accumulate(enteredAmount);
-		if(newTotal.endsWith("00")||checkBox.isSelected())
+		//if(newTotal.endsWith("00")||checkBox.isSelected())
 			newTotal=newTotal.substring(0, newTotal.length()-3);
 		totalTextField.setText(newTotal);
 		errorTextField.setText("");
 		errorTextField.setBackground(Color.white);
-		logTextArea.append(newLine+newTotal);
-		logTextArea.setCaretPosition(logTextArea.getDocument().getLength());
-		amountTextField.setText("");
+		/*logTextArea.append(newLine+newTotal);
+		logTextArea.setCaretPosition(logTextArea.getDocument().getLength());*/
+		leftTextField.setText("");
 		}
 	catch(IllegalArgumentException iae)
 		{
@@ -132,16 +168,22 @@ public void actionPerformed(ActionEvent ae) {
 //added by Kiki
 public void clear() {
 	
-	amountTextField.setText("");
+	leftTextField.setText("");
+	rightTextField.setText("");
 	totalTextField.setText("");
-	total=0;
 	logTextArea.setText("");
-	amountTextField.requestFocus();
+	leftTextField.requestFocus();
 
 }
 //added by Kiki
  public void actionListener(ActionEvent ae){
 	 
  }
+
+@Override
+public String accumulate(String amount) throws IllegalArgumentException {
+	// TODO Auto-generated method stub
+	return null;
+}
  
 }
